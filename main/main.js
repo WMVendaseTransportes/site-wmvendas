c870f42fca63403419da08a66e957c79599900d8
Alexandre Ferreira <alexandreafb45@gmail.com>
2024-03-12 15:21:49 -0300

    Banner em carrosel e novos banners

--- main/main.js
+++ main/main.js
@@ -63,3 +63,73 @@ class MobileNavbar {
         );
     }
 });
+
+/*Banner rotativo*/
+document.addEventListener("DOMContentLoaded", function() {
+  const slides = document.querySelectorAll(".carousel-slide");
+  const dotsContainer = document.querySelector(".carousel-dots");
+  let currentSlide = 0;
+  let autoSlideInterval; // Variável para armazenar o intervalo de slide automático
+  const slideInterval = 4000;
+
+  const updateDots = () => {
+    const dots = document.querySelectorAll(".carousel-dot");
+    dots.forEach((dot, index) => {
+      dot.classList.remove("active");
+      if (index === currentSlide) {
+        dot.classList.add("active");
+      }
+    });
+  };
+
+  const nextSlide = () => {
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = "block";
+    updateDots();
+    startAutoSlide(); // Reinicia o intervalo após a navegação manual
+  };
+
+  const prevSlide = () => {
+    slides[currentSlide].style.display = "none";
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].style.display = "block";
+    updateDots();
+    startAutoSlide(); // Reinicia o intervalo após a navegação manual
+  };
+
+  const startAutoSlide = () => {
+    clearInterval(autoSlideInterval); // Limpa o intervalo atual
+    autoSlideInterval = setInterval(nextSlide, slideInterval); // Inicia um novo intervalo
+  };
+
+  startAutoSlide(); // Inicia o intervalo de slide automático ao carregar a página
+
+  document.querySelector(".prev-btn").addEventListener("click", () => {
+    clearInterval(autoSlideInterval);
+    prevSlide();
+  });
+
+  document.querySelector(".next-btn").addEventListener("click", () => {
+    clearInterval(autoSlideInterval);
+    nextSlide();
+  });
+
+  // Adicionar os pontos de indicação (dots)
+  slides.forEach((_, index) => {
+    const dot = document.createElement("span");
+    dot.classList.add("carousel-dot");
+    if (index === 0) {
+      dot.classList.add("active");
+    }
+    dot.addEventListener("click", () => {
+      clearInterval(autoSlideInterval);
+      slides[currentSlide].style.display = "none";
+      currentSlide = index;
+      slides[currentSlide].style.display = "block";
+      updateDots();
+      startAutoSlide(); // Reinicia o intervalo após a navegação manual
+    });
+    dotsContainer.appendChild(dot);
+  });
+});
